25ccaa96f2aaf1517bcd024a5c51753ca3951e9d
Étienne Lévesque <etienne.levesque16@gmail.com>
2022-07-23 13:18:04 -0400

    fix destination field

--- Bot.js
+++ Bot.js
@@ -5,6 +5,23 @@ class Bot {
     this.id = id
   }
 
+  distanceFrom(debris) {
+    return Math.sqrt(Math.pow(this.tank.position[0] - debris.position[0], 2) + Math.pow(this.tank.position[1] - debris.position[1], 2))
+  }
+
+  findNearestDebris(state) {
+    return [...state.debris].sort((a, b) => {
+      const distanceA = this.distanceFrom(a)
+      const distanceB = this.distanceFrom(b)
+
+      return distanceA > distanceB ? 1 : -1
+    })[0]
+  }
+
+  destinationTo(debris) {
+    return [debris.position[0], debris.position[1] + 160]
+  }
+
   randomUpgrade() {
     const upgradeNames = Object.values(upgrades)
     return upgradeNames[Math.floor(Math.random() * upgradeNames.length)];
@@ -17,10 +34,16 @@ class Bot {
   tick(state) {
     // Program your bot here
     
+    this.tank = state.tanks[this.id]
+
+    console.log([this.tank.max_hp, this.tank.current_hp])
+    
+    const nearestDebris = this.findNearestDebris(state)
+
     return {
-      destination: this.randomPosition(state),
-      target: this.randomPosition(state),
-      purchase: this.randomUpgrade()
+      destination: nearestDebris.position,
+      target: nearestDebris.position,
+      purchase: upgrades.maxHp
     }
   }
 }
